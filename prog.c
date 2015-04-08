5609e7cfe2c28119a2aa1a258ecda0f99008866e
Jonas Höglund <jonas.hoglund@embeddednation.com>
2015-04-08 16:18:25 +0200

    check busmail rx packet

--- prog.c
+++ prog.c
@@ -26,7 +26,50 @@
 #include "util.h"
 #include "prog.h"
 
+#define BUSMAIL_PACKET_HEADER 0x10
+#define BUSMAIL_PACKET_OVER_HEAD 4
 
+static int inspect_rx(event_t *e) {
+	
+	uint32_t data_size = 0, i;
+	uint8_t crc = 0, crc_calc = 0;
+	
+	/* Check header */
+	if (e->in[0] =! BUSMAIL_PACKET_HEADER) {
+		printf("Drop packet: no header\n");
+		return -1;
+	}
+
+	/* Check size */
+	if (e->incount < BUSMAIL_PACKET_OVER_HEAD) {
+		printf("Drop packet: packet size smaller then BUSMAIL_PACKET_OVER_HEAD %d < %d\n",
+		       e->incount, BUSMAIL_PACKET_OVER_HEAD);
+		return -1;
+	}
+
+	/* Do we have a full packet? */
+	data_size = (((uint32_t) e->in[1] << 8) | e->in[2]);
+	if (e->incount < (data_size + BUSMAIL_PACKET_OVER_HEAD)) {
+		printf("Drop packet: not a full packet incount: %d < packet size: %d\n",
+		       e->incount, data_size + BUSMAIL_PACKET_OVER_HEAD);
+		return -1;
+	}
+	
+	/* Read packet checksum */
+	crc = (( (uint8_t) e->in[e->incount - 1]));
+
+	/* Calculate checksum over data portion */
+	for (i = 0; i < data_size; i++) {
+		crc_calc += e->in[i + 3];
+	}
+
+	if (crc != crc_calc) {
+		printf("Drop packet: bad packet checksum: %x != %x\n", crc, crc_calc);
+		return -1;
+	}
+
+	return 0;
+}
 
 
 
@@ -42,7 +85,13 @@ void init_prog_state(int dect_fd) {
 
 void handle_prog_package(event_t *e) {
 
-	
+	util_dump(e->in, e->incount, "[READ]");
+
+	if (inspect_rx(e) < 0) {
+		printf("dropped packet\n");
+	} 
+
+
 	switch (e->in[0]) {
 
 	default:
